beebbcb3285ea4268314321854c663f81e8b0367
Nick Bansal <extreme_wired@msn.com>
2018-08-26 19:15:50 +0100

    ToDo List with delete function

--- src/App.js
+++ src/App.js
@@ -4,39 +4,49 @@ import './App.css';
 
 class App extends Component {
   state = {
-    inputText: '',
+    inputText: 'What needs to be done?',
     data: [],
   }
 
-  
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const data = [...this.state.data, this.state.inputText]
+    this.setState({
+      data, inputText: '',
+    })
+  }
+
+  deleteTodo = (index) => {
+    const data = [...this.state.data];
+    data.splice(index, 1); 
+    this.setState({
+      data,
+    });
+    console.log(data)
+  }
 
 
   render() {
 
     const newElement = [...this.state.data].map((el, index) => {
-      return <li key={index}>{el}</li>
+      return <li key={index}
+      onClick={() => {this.deleteTodo(index)}}>
+      {el}
+      </li>
     })
 
     return (
       <div className='App'>
         <h1>ToDo List</h1>
         
-        <form onSubmit = {(e) => {
-          e.preventDefault();
-          const data = [...this.state.data, this.state.inputText]
-          this.setState({
-            data, inputText: '',
-          })
-        }}>
-
-          <input 
-          type = 'text'  
+        <form onSubmit = {this.handleSubmit} >
+          <input type = 'text'  
           value= {this.state.inputText}
           onChange = {(e) => {
             this.setState({inputText: e.target.value})
           }}
           />
-
+          <button type="submit">SAVE</button>
         </form>
 
         <div className='ListItems'>
