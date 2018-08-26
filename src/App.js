253bcd0d73147cc1a39c3a0d97eca55334d8ab08
Nick Bansal <extreme_wired@msn.com>
2018-08-26 17:58:27 +0100

    Final Simple ToDo

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ class App extends Component {
         <h1>ToDo List</h1>
         
         <form onSubmit = {(e) => {
-          console.log(this.state.data)
           e.preventDefault();
           const data = [...this.state.data, this.state.inputText]
           this.setState({
@@ -40,9 +39,11 @@ class App extends Component {
 
         </form>
 
-        <ol>
-          {newElement}
-        </ol>
+        <div className='ListItems'>
+          <ol>
+            {newElement}
+          </ol>
+        </div>
 
       </div>
     );
